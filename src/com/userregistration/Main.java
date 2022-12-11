17a30c5686138ba814366e4a9ee330f14c7d6b4f
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-11 17:17:03 +0530

    ADDED : Code to validate email.

--- src/com/userregistration/Main.java
+++ src/com/userregistration/Main.java
@@ -10,5 +10,6 @@ public class Main {
 
     public void validatingUserRegistration() {
         System.out.println("\nValidating FirstName and LastName " + Pattern.matches("^[A-Z]{1}[a-z]{2,}[\s][A-Z]{1}[a-z]{2,}$", "Prashik Kamble"));
+        System.out.println("\nValidating Email " + Pattern.matches("^((([a-z]{3})|([a-z]{3}[.][a-z]{3}))[@][bl]{2}[.][co]{2})$|^((([a-z]{3})|([a-z]{3}[.][a-z]{3}))[@][bl]{2}[.][co]{2}[.][in]{2})$", "abc.xyz@bl.co.in"));
     }
 }
