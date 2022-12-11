7f8412997e865e7e6cf0543e97f3fc714ea18923
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-11 16:50:33 +0530

    ADDED : Code to validate first name.

--- src/com/userregistration/Main.java
+++ src/com/userregistration/Main.java
@@ -1,7 +1,14 @@
 package com.userregistration;
 
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Problem!");
+        new Main().validatingUserRegistration();
+    }
+
+    public void validatingUserRegistration() {
+        System.out.println("\nValidating FirstName " + Pattern.matches("^[A-Z]{1}[a-z]{2,}$", "Prashik"));
     }
 }
