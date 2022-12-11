1225f58891f7336beb38dfe626c1a11251630175
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-11 17:22:08 +0530

    ADDED : Code to validate mobile format.

--- src/com/userregistration/Main.java
+++ src/com/userregistration/Main.java
@@ -11,5 +11,6 @@ public class Main {
     public void validatingUserRegistration() {
         System.out.println("\nValidating FirstName and LastName " + Pattern.matches("^[A-Z]{1}[a-z]{2,}[\s][A-Z]{1}[a-z]{2,}$", "Prashik Kamble"));
         System.out.println("\nValidating Email " + Pattern.matches("^((([a-z]{3})|([a-z]{3}[.][a-z]{3}))[@][bl]{2}[.][co]{2})$|^((([a-z]{3})|([a-z]{3}[.][a-z]{3}))[@][bl]{2}[.][co]{2}[.][in]{2})$", "abc.xyz@bl.co.in"));
+        System.out.println("\nValidating Mobile Format " + Pattern.matches("^[0-9]{2}[\\s][0-9]{10}$", "91 9919819801"));
     }
 }
