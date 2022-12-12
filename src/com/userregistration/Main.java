75503646969ced65bf6d5e0a3c78a132e59063fa
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-12 15:30:41 +0530

    MODIFIED : Code so that the Password has exactly one special character.

--- src/com/userregistration/Main.java
+++ src/com/userregistration/Main.java
@@ -1,8 +1,10 @@
 package com.userregistration;
 
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Problem!");
         new Main().validatingUserRegistration();
@@ -10,8 +12,8 @@ public class Main {
 
     public void validatingUserRegistration() {
         System.out.println("\nValidating FirstName and LastName " + Pattern.matches("^[A-Z]{1}[a-z]{2,}[\s][A-Z]{1}[a-z]{2,}$", "Prashik Kamble"));
-        System.out.println("\nValidating Email " + Pattern.matches("^((([a-z]{3})|([a-z]{3}[.][a-z]{3}))[@][bl]{2}[.][co]{2})$|^((([a-z]{3})|([a-z]{3}[.][a-z]{3}))[@][bl]{2}[.][co]{2}[.][in]{2})$", "abc.xyz@bl.co.in"));
+        System.out.println("\nValidating Email " + Pattern.matches("^((([a-z]{3})|([a-z]{3}[.][a-z]{3}))[@][bl]{2}[.]([co]{2}|[co]{2}[.][in]{2}))$", "abc.xyz@bl.co.in"));
         System.out.println("\nValidating Mobile Format " + Pattern.matches("^[0-9]{2}[\\s][0-9]{10}$", "91 9919819801"));
-        System.out.println("\nPassword should have minimum 8 characters and at least 1 UpperCase letter and 1 Numeric Number " + Pattern.matches("^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]){8,}$", "a1AeB3he"));
+        System.out.println("\nPassword should have minimum 8 characters and at least 1 UpperCase letter and 1 Numeric Number and exactly One Special character " + Pattern.matches("^(?=[^!@#$%^&*]*[!@#$%^&*]([^!@#$%^&*])*$)(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9(!@#$%^&*)]){8,}$", "a1aEWe@3he"));
     }
 }
